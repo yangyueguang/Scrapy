8af0f092e43f0da120c87020f60a9555857006f3
yangyueguang <2829969299@qq.com>
2020-02-16 23:50:25 +0800

    update

--- project/spiders/redis_spider.py
+++ project/spiders/redis_spider.py
@@ -2,6 +2,7 @@
 from scrapy.spiders import Rule
 from scrapy.linkextractors import LinkExtractor
 from scrapy_redis.spiders import RedisCrawlSpider
+from scrapy_redis.spiders import RedisSpider
 from project.items import YYItem
 '''
 # 启动redis
@@ -15,7 +16,36 @@ ITEM_PIPELINES = {
     'project.pipelines.YYPipeline': 300,
     'scrapy_redis.pipelines.RedisPipeline': 400,
 }
+
+# 使用
+先连接同一个redis服务器
+1. 各个地址启动爬虫
+scrapy runspider $spider_py_path
+2. 在redis客户端发送一条消息
+> lpush $redis_key $start_url
 '''
+
+class MySpider(RedisSpider):
+    name = 'YY'
+    #allowed_domains = ['youyuan.com']
+    #start_urls = ['http://www.youyuan.com/find/beijing/mm18-25/advance-0-0-0-0-0-0-0/p1/']
+    redis_key = "yyspider:start_urls"
+
+    def __init__(self, *args, **kwargs):
+        domain = kwargs.pop('domain', '')
+        self.allowed_domains = filter(None, domain.split(','))
+        super(YySpider, self).__init__(*args, **kwargs)
+
+    def parse(self, response):
+        print(response.text)
+        item = YYItem()
+        item['name'] = ''
+        item['unit'] = ''
+        item['time'] = '2020-01-01 00:00'
+        item['address'] = ''
+        item['sources'] = self.name
+        yield item
+
 
 class YySpider(RedisCrawlSpider):
     name = 'YY'
